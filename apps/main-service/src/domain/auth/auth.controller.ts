3f3bfe374a159bff5e511cd724702e737b44e4e8
Hudson Nguyen <hungson.nguyen811@gmail.com>
2024-10-30 04:37:14 -0500

    feat: auth module setup

---
+++ apps/main-service/src/domain/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { Public } from '../../common/decorator/public.decorator';
+import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
+@Controller()
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  // TODO: swagger
+  @Public()
+  @Post('auth/sign-in')
+  signIn(@Body() data: SignInDto) {
+    return this.authService.signIn(data.email, data.password);
+  }
+}
--- apps/main-service/src/domain/domain.module.ts
+++ apps/main-service/src/domain/domain.module.ts
@@ -1,6 +1,7 @@
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
 })
 export class DomainModule {}
