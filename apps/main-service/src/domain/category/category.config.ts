9ae750d9c724cc06f738d8218233d7a5c3eb74b7
Hudson Nguyen <hungson.nguyen811@gmail.com>
2024-12-07 21:21:59 -0600

    chore(categories): add more categories for the app

---
+++ apps/main-service/src/domain/category/category.config.ts
@@ -0,0 +1,11 @@
+export const CATEGORIES = [
+  { name: 'cabin', description: 'cabin' },
+  { name: 'airstream', description: 'airstream' },
+  { name: 'tent', description: 'tent' },
+  { name: 'warehouse', description: 'warehouse' },
+  { name: 'farmhouse', description: 'farmhouse' },
+  { name: 'yurt', description: 'yurt' },
+  { name: 'safari_tent', description: 'safari_tent' },
+  { name: 'converted_barn', description: 'converted_barn' },
+  // Add more categories here
+] as const;
